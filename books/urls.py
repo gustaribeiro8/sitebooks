9b8f37a4c69fbc3454b49ea0178019f492a5d618
Gustavo Ribeiro <gustavo.ribeiro10@usp.br>
2023-11-19 18:41:23 -0300

    Criar aplicação CRUD de livros

--- books/urls.py
+++ books/urls.py
@@ -4,8 +4,11 @@ from . import views
 
 app_name = 'books'
 urlpatterns = [
-    path('', views.list_books, name='index'),
+    path('', views.BookListView.as_view(), name='index'),
     path('search/', views.search_books, name='search'),
     path('<int:book_id>/', views.detail_book, name='detail'),
     path('create/', views.create_book, name='create'),
+    path('update/<int:book_id>/', views.update_book, name='update'),
+    path('delete/<int:book_id>/', views.delete_book, name='delete'),
+
 ]
--- books/views.py
+++ books/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponseRedirect
 from .temp_data import book_data
 from .models import Book
-from django.shortcuts import render
 from django.urls import reverse
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
 
 def detail_book(request, book_id):
-    context = {'book': book_data[book_id - 1]}
+    book = get_object_or_404(Book, pk=book_id)
+    context = {'book': book}
     return render(request, 'books/detail.html', context)
 
 def list_books(request):
@@ -16,24 +18,51 @@ def list_books(request):
 def search_books(request):
     context = {}
     if request.GET.get('query', False):
-        context = {
-            "book_list": [
-                m for m in book_data
-                if request.GET['query'].lower() in m['name'].lower()
-            ]
-        }
+        search_term = request.GET['query'].lower()
+        book_list = Book.objects.filter(name__icontains=search_term)
+        context = {"book_list": book_list}
     return render(request, 'books/search.html', context)
 
 def create_book(request):
     if request.method == 'POST':
-        book_data.append({
-            'name': request.POST['name'],
-            'release_year': request.POST['release_year'],
-            'poster_url': request.POST['poster_url']
-        })
+        book_name = request.POST['name']
+        book_release_year = request.POST['release_year']
+        book_poster_url = request.POST['poster_url']
+        book = Book(name=book_name,
+                      release_year=book_release_year,
+                      poster_url=book_poster_url)
+        book.save()
         return HttpResponseRedirect(
-            reverse('books:detail', args=(len(book_data), )))
+            reverse('books:detail', args=(book.id, )))
     else:
         return render(request, 'books/create.html', {})
 
 
+def update_book(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+
+    if request.method == "POST":
+        book.name = request.POST['name']
+        book.release_year = request.POST['release_year']
+        book_poster_url = request.POST['poster_url']
+        book.save()
+        return HttpResponseRedirect(
+            reverse('books:detail', args=(book.id, )))
+
+    context = {'book': book}
+    return render(request, 'books/update.html', context)
+
+
+def delete_books(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+
+    if request.method == "POST":
+        book.delete()
+        return HttpResponseRedirect(reverse('books:index'))
+
+    context = {'book': book}
+    return render(request, 'books/delete.html', context)
+
+class BookListView(generic.ListView):
+    model = Book
+    template_name = 'books/index.html'
